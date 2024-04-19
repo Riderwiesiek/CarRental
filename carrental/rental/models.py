59fa39bae6e6ccdb53bd2b83b733a0b00d084b7c
Riderwiesiek <116362927+Riderwiesiek@users.noreply.github.com>
2024-04-19 18:44:28 +0200

    19.04.2024

--- carrental/rental/models.py
+++ carrental/rental/models.py
@@ -4,23 +4,22 @@ class Equipment(models.Model):
     equipment = models.CharField(max_length=50)
 
 class Car(models.Model):
-    GEARBOX_TYPES = [
-    ("automatyczna", "Automatyczna"),
-    ("manualna", "Manualna"),
-    ("polautomatyczna", "Polautomatyczna"),
-    ]
     ENGINE_TYPES = [
-    ("benzynowy", "Benzynowy"),
-    ("diesel", "Diesel"),
-    ("hybrydowy", "Hybrydowy"),
-    ("elektryczny", "Elektryczny"),
-    ("wodorowy", "Wodorowy")
+        ("benzynowy", "Benzynowy"),
+        ("diesel", "Diesel"),
+        ("hybrydowy", "Hybrydowy"),
+        ("elektryczny", "Elektryczny"),
+        ("wodorowy", "Wodorowy")
     ]
-
+    GEARBOX_TYPES = [
+        ("automatyczna", "Automatyczna"),
+        ("manualna", "Manualna"),
+        ("polautomatyczna", "Polautomatyczna"),
+    ]
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     engine_type = models.CharField(max_length=20, choices=ENGINE_TYPES)
-    seats_count = models.PositiveSmallIntegerField()
+    seats_count = models.PositiveSmallIntegerField() 
     dors_count = models.PositiveSmallIntegerField()
     fuel_usage = models.FloatField()
     engine_power = models.PositiveSmallIntegerField()
@@ -34,12 +33,12 @@ class User(models.Model):
 
 class Order(models.Model):
     PAYMENT_METHODS = [
-    ("karta", "Karta"),
-    ("gotowka", "Gotowka"),
-    ("przelew", "Przelew"),
-    ("blik", "Blik"),
+        ("karta", "Karta"),
+        ("gotowka", "Gotowka"),
+        ("przelew", "Przelew"),
+        ("blik", "Blik"),
     ]
-    customer = models.ForeignKey(User, on_delate=models.RESTRICT)
+    customer = models.ForeignKey(User, on_delete=models.RESTRICT)
     car = models.ForeignKey(Car, on_delete=models.RESTRICT)
     order_value = models.DecimalField(max_digits=10, decimal_places=2)
     declared_order_duration = models.DurationField()
@@ -48,5 +47,3 @@ class Order(models.Model):
     deposit = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
     payment_status = models.BooleanField()
-
-
--- carrental/rental/views.py
+++ carrental/rental/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'index.html.jinja')
+    return render(request,'index.html.jinja', {'index' : index})
 
 def cars(request):
     return render(request,'cars.html.jinja')
